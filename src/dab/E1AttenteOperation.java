6b84c2c282135a053fcc4e5813c856a92b2e1e0c
Dyanek <nathanael.omnes@gmail.com>
2019-02-05 11:27:22 +0100

    virement commencé

--- src/dab/E1AttenteOperation.java
+++ src/dab/E1AttenteOperation.java
@@ -4,41 +4,45 @@ import java.util.Scanner;
 
 public class E1AttenteOperation extends Etat {
 
-	public void afficheMenu() {
-		System.out.println(" +-------------MENU----------------+");
-		System.out.println(" | 1. Consultation.                |");
-		System.out.println(" | 2. Virement.                    |");
-		System.out.println(" | 3. Retirer votre carte.(Quitter)|");
-		System.out.println(" +---------------------------------+");
-	}
+    public void afficheMenu() {
+        System.out.println(" +-------------MENU----------------+");
+        System.out.println(" | 1. Consultation.                |");
+        System.out.println(" | 2. Virement.                    |");
+        System.out.println(" | 3. Retirer votre carte.(Quitter)|");
+        System.out.println(" +---------------------------------+");
+    }
 
-	public static int getChoixMenu(int min, int max) {
-		System.out.print("Tapez votre choix : ");
-		Scanner input = new Scanner(System.in);
-		int choix = input.nextInt();
+    public static int getChoixMenu(int min, int max) {
+        System.out.print("Tapez votre choix : ");
+        Scanner input = new Scanner(System.in);
+        int choix = input.nextInt();
 
-		while (choix < min || choix > max) {
-			System.out.print("Tapez votre choix entre " + min + " et " + max + " svp : ");
-			choix = input.nextInt();
-		}
+        while (choix < min || choix > max) {
+            System.out.print("Tapez votre choix entre " + min + " et " + max + " svp : ");
+            choix = input.nextInt();
+        }
 
-		return choix;
-	}
+        return choix;
+    }
 
-	public void traitement(Distrib distrib) throws Exception {
-		afficheMenu();
-		int choix = getChoixMenu(1, 3);
+    public void traitement(Distrib distrib) throws Exception {
+        afficheMenu();
+        int choix = getChoixMenu(1, 3);
 
-		switch (choix) {
-		case 1:
-			distrib.changeEtat(new E3AttenteRecuperationComptesConsultation());
-		case 3:
-			distrib.changeEtat(new E2EtatFinal());
-			break;
-		default:
-			this.traitement(distrib);
-			break;
-		}
+        switch (choix) {
+            case 1:
+                distrib.changeEtat(new E3AttenteRecuperationComptesConsultation());
+                break;
+            case 2:
+				distrib.changeEtat(new E6AttenteRecuperationComptesVirement());
+                break;
+            case 3:
+                distrib.changeEtat(new E2EtatFinal());
+                break;
+            default:
+                this.traitement(distrib);
+                break;
+        }
 
-	}
+    }
 }
---
+++ src/dab/E6AttenteRecuperationComptesVirement.java
@@ -0,0 +1,7 @@
+package dab;
+
+public class E6AttenteRecuperationComptesVirement extends Etat {
+    public void traitement(Distrib distrib) throws Exception {
+
+    }
+}
